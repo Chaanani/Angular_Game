90a8f1a3993c41f612b06be518adb36f53312cfe
Youness <Youness.chaanani7@gmail.com>
2024-12-08 22:21:12 +0100

    add project Angular

---
+++ src/app/pokemon/pokemon.module.ts
@@ -0,0 +1,35 @@
+import { FormsModule } from '@angular/forms';
+import { PokemonFormComponent } from './pokemon-form/pokemon-form.component';
+import { EditPokemonComponent } from './edit-pokemon/edit-pokemon.component';
+import { AddPokemonComponent } from './add-pokemon/add-pokemon.component';
+import { SearchPokemonComponent } from './search-pokemon/search-pokemon.component';
+import { LoaderComponent } from './loader/loader.component';
+import { AuthGuard } from '../auth.guard';
+
+const pokemonRoutes: Routes = [
+  { path: 'edit/pokemon/:id', component: EditPokemonComponent, canActivate: [AuthGuard] },
+  { path: 'pokemon/add', component: AddPokemonComponent, canActivate: [AuthGuard] },
+  { path: 'pokemons', component: ListPokemonComponent, canActivate: [AuthGuard] },
+  { path: 'pokemon/:id', component: DetailPokemonComponent, canActivate: [AuthGuard] }
+];
+
+@NgModule({
+  declarations: [
+    ListPokemonComponent,
+    DetailPokemonComponent,
+    BorderCardDirective,
+    PokemonTypeColorPipe,
+    PokemonFormComponent,
+    EditPokemonComponent,
+    AddPokemonComponent,
+    SearchPokemonComponent,
+    LoaderComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(pokemonRoutes)
+  ],
+  providers: [PokemonService]
+})
+export class PokemonModule { }
